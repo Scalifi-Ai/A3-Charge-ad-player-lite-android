ac3f03b13a02dfe1a962dd98c1d426336e3f6f97
matrixdev <www.matrix.dev@gmail.com>
2025-07-16 17:12:00 +0300

    version 2.0.1

--- app/src/main/java/com/adservrs/adplayer/lite/example/MainScreen.kt
+++ app/src/main/java/com/adservrs/adplayer/lite/example/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.adservrs.adplayer.lite.example
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
@@ -12,12 +13,20 @@ fun MainScreen(modifier: Modifier = Modifier) {
         AndroidView(
             factory = {
                 val view = AdPlayerView(it)
-                view.load(pubId = BuildConfig.AV_PUB_ID, tagId = BuildConfig.AV_TAG_ID)
+//                view.load(
+//                    pubId = "565c56d3181f46bd608b459a",
+//                    tagId = "67dd7d7835b4a37b880f2b3c",
+//                )
+                view.load(
+                    pubId = "67c61463b9541df7de0d1971",
+                    tagId = "67c61770cabacd39ac031960",
+                )
                 view
             },
             onRelease = {
                 it.release()
             },
+            modifier = Modifier.fillMaxSize(),
         )
     }
 }
