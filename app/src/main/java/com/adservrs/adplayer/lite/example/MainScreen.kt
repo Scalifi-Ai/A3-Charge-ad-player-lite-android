2e629a527515e3f4c219de189a2ab9425f77bbed
Aryan Chugh <chugharyan816@gmail.com>
2025-08-20 18:00:19 +0530

    Minor bug fixes

--- app/src/main/java/com/adservrs/adplayer/lite/example/MainScreen.kt
+++ app/src/main/java/com/adservrs/adplayer/lite/example/MainScreen.kt
@@ -13,13 +13,9 @@ fun MainScreen(modifier: Modifier = Modifier) {
         AndroidView(
             factory = {
                 val view = AdPlayerView(it)
-//                view.load(
-//                    pubId = "565c56d3181f46bd608b459a",
-//                    tagId = "67dd7d7835b4a37b880f2b3c",
-//                )
                 view.load(
-                    pubId = "67c61463b9541df7de0d1971",
-                    tagId = "67c61770cabacd39ac031960",
+                    pubId = "68540ce88ee8d56dbe0d78a6",
+                    tagId = "689497109dc347458b0c614a",
                 )
                 view
             },
